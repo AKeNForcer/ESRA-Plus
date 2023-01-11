e1c82dc7cc48ea59cc140ec812fc319045d06b35
ESRA <MY_NAME@example.com>
2023-01-11 08:59:45 +0000

    chang dev port

--- esra-backend/src/main.ts
+++ esra-backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 3000;
+  const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 3001;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
