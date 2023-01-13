3c32858357891cf7cfe4320f226564cece7fc45f
AKeNForcer <nonlnw62543@gmail.com>
2023-01-14 05:22:21 +0700

    [UPDATE] search bar design

--- esra-frontend/components/search/search-component.tsx
+++ esra-frontend/components/search/search-component.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export const SearchComponent = () => {
   const [query, setQuery] = useState("");
-  const [completion, setCompletion] = useState<Array<{[key: string]: any}>>([]);
+  const [completion, setCompletion] = useState<Array<{ [key: string]: any }>>([]);
 
   const handleQuery = (event: any) => {
     const newQuery = event.target.value;
@@ -19,43 +19,33 @@ export const SearchComponent = () => {
     } else {
       setCompletion([]);
     }
-
-    // if (newQuery.length == 0) {
-    //   setCompletion([]);
-    // } else {
-    //   setCompletion([
-    //     {
-    //       "detail": null,
-    //       "paperId": "2209.07244",
-    //       "rank": 1,
-    //       "score": 13900236,
-    //       "title": "Linear Transformations for Cross-lingual Sentiment Analysis sddlsdld slsdl lds ldsa l l d d s s ssd"
-    //     },
-    //     {
-    //         "detail": null,
-    //         "paperId": "1908.06121",
-    //         "rank": 2,
-    //         "score": 13537011,
-    //         "title": "CFO: A Framework for Building Production NLP Systems"
-    //     }
-    //   ]);
-    // }
   }
 
   return <>
     <div className='h-[400px] w-full max-w-[700px] text-base'> {/*absolute inset-y-[345px]  z-20*/}
-      <div className="flex h-12 w-full items-center bg-white rounded-3xl border-[1px] border-gray-300 text-gray-400 relative z-20">
-        <Search className='ml-3'/>
-        <input className='ml-3 mx-5 text-gray-600 w-full focus:outline-none bg-transparent' type="text" onChange={handleQuery} />
+      <div className={`flex-col h-12 w-full items-center bg-transparent rounded-3xl border-[1px] border-gray-300 relative z-20 ${completion.length > 0 ? '' : 'focus-within:shadow-md'}`}>
+        <div className="flex h-12 w-full items-center relative z-30">
+          <div className="flex h-12 w-full items-center rounded-3xl text-gray-400 relative z-30">
+            <Search className='ml-3' />
+            <input className='ml-3 mx-5 text-gray-600 w-full focus:outline-none bg-transparent' type="text" onChange={handleQuery} />
+          </div>
+        </div>
+        { 
+          completion.length > 0 ? 
+          <div className="flex h-6 w-full bg-white relative z-20 -inset-y-6">
+            <div className="h-full w-full mx-3 border-b-[1px]"/>
+          </div> : null
+        }
       </div>
       {
         completion.length > 0 ?
-          <ul className="flex flex-col h-auto max-h-[600px] w-full short:max-h-[360px] rxs:max-h-[480px] overflow-auto items-start pt-[55px] pb-[20px] rounded-3xl border-[1px] border-gray-300 text-gray-400 shadow-md text-left relative -inset-y-[48px] z-10">
-            {completion.map((res: {[key: string]: any}) => <SearchResult title={res["title"]} key={res["paperId"]}/>)}
-          </ul> :
-          null
+          <div className="w-full shadow-md text-left relative -inset-y-[48px] z-10 rounded-3xl border-[1px] border-gray-300">
+            <ul className="flex flex-col h-auto w-full max-h-[415px] short:max-h-[360px] rxs:max-h-[360px] overflow-auto items-start mt-[45px] pt-[10px] mb-[20px] text-gray-400">
+              {completion.map((res: { [key: string]: any }) => <SearchResult title={res["title"]} key={res["paperId"]} />)}
+            </ul>
+          </div> : null
       }
     </div>
     {/* <div className='h-[400px] w-full max-w-[700px] bg-red-200'></div> */}
-  </>
+    </>
 }
