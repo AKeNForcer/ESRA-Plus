51ec39bc7fe15d81577cf300f351f0439710f9bf
AKeNForcer <nonlnw62543@gmail.com>
2023-01-27 23:23:39 +0700

    [UPDATE] implement related papers logic

--- esra-frontend/pages/paper/index.tsx
+++ esra-frontend/pages/paper/index.tsx
@@ -18,6 +18,7 @@ const PaperPage: NextPage = () => {
   const [paper, setPaper] = useState<{ [key: string]: any } | undefined>(undefined);
   const [showMinimal, setShowMinimal] = useState(false);
   const [showMinimalBar, setShowMinimalBar] = useState(false);
+  const [relatedPapers, setRelatedPapers] = useState<Array<{[key: string]: any}>>([]);
 
   const origin = process.env.NEXT_PUBLIC_DEV_URL ?? (
     typeof window !== 'undefined' && window.location.origin
@@ -26,12 +27,25 @@ const PaperPage: NextPage = () => {
 
   useEffect(() => {
     console.log(`paper or query changed: ${paperId}, ${query}`);
+    setRelatedPapers([]);
     if (paperId) {
       const PAPER_URL = new URL('paper', origin).toString();
       axios.get(PAPER_URL, { params: { query: query, paperId } })
         .then(response => {
-          setPaper({paperId, ...response.data.result});
+          setPaper({ paperId, ...response.data.result });
           console.log(response.data.result);
+          const RELATE_URL = new URL('search', origin).toString();
+          axios.get(RELATE_URL, { params: { query: `${query} ${response.data.result['title']}`, limit: 6 } })
+            .then(response => {
+              const resArr: Array<{[key: string]: any}> = []
+              for (const res of response.data.result){
+                if (res.paperId === paperId) continue;
+                resArr.push(res)
+                if (resArr.length >= 5) break;
+              }
+              setRelatedPapers(resArr);
+              console.log(resArr);
+            });
         })
         .catch(error => {
           if (error.response.status === 404) {
@@ -62,10 +76,10 @@ const PaperPage: NextPage = () => {
         <title>{paperId}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className={`${showMinimal ? 'fixed' : ''} w-full z-50 h-0 -inset-y-[30px]`}>
         <div className="flex w-full flex-col h-0 items-center px-5 pt-10 justify-start">
-          <SearchComponent currPage="search"/> 
+          <SearchComponent currPage="search" />
         </div>
       </div>
 
@@ -78,58 +92,55 @@ const PaperPage: NextPage = () => {
 
         {
           showMinimalBar ?
-          <div className="fixed flex items-center w-full h-[68px] border-b-[1px] border-gray-300 bg-white"/> : null
+            <div className="fixed flex items-center w-full h-[68px] border-b-[1px] border-gray-300 bg-white" /> : null
         }
 
         <main className='flex flex-col show-logo:flex-row w-full items-center show-logo:items-start justify-center text-gray-600 px-5 gap-5 py-4'>
           {
             notFound ?
-            <div className='flex flex-col w-full gap-10 py-20'>
-              <h2 className='text-9xl font-light text-gray-300'>404</h2>
-              <h3 className='text-gray-400'>
-                Paper '{paperId}' is not found in our database right now!
-              </h3>
-            </div> :
-            <>
-              <div className='flex flex-col w-full max-w-[750px] show-logo:max-w-[950px] items-center gap-3 pt-5 show-logo:pt-0'>
-                <ul className='flex flex-col items-center justify-center w-full gap-3'>
-                  { (query !== undefined && paper !== undefined) ? <ResultPaper query={query} result={paper}/> : null }
-                </ul>
-              </div>
-              <div className='flex flex-col w-full max-w-[750px] show-logo:max-w-[300px] items-center gap-3'>
-                <div className='flex flex-row w-full justify-center items-center gap-3'>
-                  <a href={paper ? paper["pdf"] : ''} target="_blank" className='flex justify-center items-center h-12 w-full border-[1px] p-3 bg-cyan-800 text-white hover:bg-opacity-80 hover:shadow-md'>
-                    PDF
-                  </a>
-                  <a href={paper ? paper["arxiv"] : ''} target="_blank" className='flex justify-center items-center h-12 w-full border-[1px] p-3 bg-red-800 text-white hover:bg-opacity-80 hover:shadow-md'>
-                    ArXiv
-                  </a>
+              <div className='flex flex-col w-full gap-10 py-20'>
+                <h2 className='text-9xl font-light text-gray-300'>404</h2>
+                <h3 className='text-gray-400'>
+                  Paper '{paperId}' is not found in our database right now!
+                </h3>
+              </div> :
+              <>
+                <div className='flex flex-col w-full max-w-[750px] show-logo:max-w-[950px] items-center gap-3 pt-5 show-logo:pt-0'>
+                  <ul className='flex flex-col items-center justify-center w-full gap-3'>
+                    {(query !== undefined && paper !== undefined) ? <ResultPaper query={query} result={paper} /> : null}
+                  </ul>
                 </div>
-                <div className='flex flex-col w-full justify-start items-start p-3 border-[1px] text-start hover:shadow-md'>
-                  <h3 className='pl-1 pb-3'>Related Papers</h3>
-                  <Link href={`/google`}>
-                    <div>
-
-                    </div>
-                    <h4 className='mx-2 p-3 font-semibold text-cyan-900 hover:underline border-t-[1px]'>
-                      Learning Deformable Object Manipulation from Expert Demonstrations
-                    </h4>
-                    <h4 className='mx-2 p-3 font-semibold text-cyan-900 hover:underline border-t-[1px]'>
-                      Comparison of Speech Activity Detection Techniques for Speaker Recognition
-                    </h4>
-                    <h4 className='mx-2 p-3 font-semibold text-cyan-900 hover:underline border-t-[1px]'>
-                      Worst-case Throughput Analysis for Parametric Rate and Parametric Actor Execution Time Scenario-Aware Dataflow Graphs
-                    </h4>
-                    <h4 className='mx-2 p-3 font-semibold text-cyan-900 hover:underline border-t-[1px]'>
-                      Learning Deformable Object Manipulation from Expert Demonstrations
-                    </h4>
-                    <h4 className='mx-2 p-3 font-semibold text-cyan-900 hover:underline border-t-[1px]'>
-                      Comparison of Speech Activity Detection Techniques for Speaker Recognition
-                    </h4>
-                  </Link>
+                <div className='flex flex-col w-full max-w-[750px] show-logo:max-w-[300px] items-center gap-3'>
+                  <div className='flex flex-row w-full justify-center items-center gap-3'>
+                    <a href={paper ? paper["pdf"] : ''} target="_blank" className='flex justify-center items-center h-12 w-full border-[1px] p-3 bg-cyan-800 text-white hover:bg-opacity-80 hover:shadow-md hover:underline'>
+                      PDF
+                    </a>
+                    <a href={paper ? paper["arxiv"] : ''} target="_blank" className='flex justify-center items-center h-12 w-full border-[1px] p-3 bg-red-800 text-white hover:bg-opacity-80 hover:shadow-md hover:underline'>
+                      ArXiv
+                    </a>
+                  </div>
+                  <div className='flex flex-col w-full justify-start items-start p-3 border-[1px] text-start hover:shadow-md'>
+                    <h3 className='pl-1 pb-3'>Related Papers</h3>
+                    {
+                      relatedPapers && relatedPapers.length > 0 ?
+                      relatedPapers.map((res: { [key: string]: any }) => (
+                        <Link href={`/paper?paperId=${res['paperId']}&query=${query} ${res['title']}`}>
+                          <h4 className='p-3 w-full font-semibold text-cyan-900 hover:underline border-t-[1px]'>
+                            {res['title']}
+                          </h4>
+                        </Link>
+                      )) :
+                      Array(5).fill(
+                        <h4 className='flex flex-col pt-5 p-3 w-full font-semibold text-cyan-900 hover:underline border-t-[1px] gap-2'>
+                          <div className='w-11/12 h-4 bg-gray-200 animate-pulse rounded-full' />
+                          <div className='w-3/4 h-4 bg-gray-200 animate-pulse rounded-full' />
+                          <div className='w-4/5 h-4 bg-gray-200 animate-pulse rounded-full' />
+                        </h4>
+                      )
+                    }
+                  </div>
                 </div>
-              </div>
-            </>
+              </>
           }
         </main>
       </div>
