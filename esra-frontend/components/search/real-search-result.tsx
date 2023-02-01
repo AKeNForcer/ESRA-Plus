e9ca20b198384f44d3bb3a961972307a25d4cb97
AKeNForcer <nonlnw62543@gmail.com>
2023-02-01 18:09:20 +0700

    [ADD] dummy foldable explaination

--- esra-frontend/components/search/real-search-result.tsx
+++ esra-frontend/components/search/real-search-result.tsx
@@ -17,14 +17,6 @@ export const RealSearchResult = (props: any) => {
       <div className='flex w-full font-ligeht'>
         <div className='flex flex-wrap justify-start w-full gap-y-1 gap-x-4 pb-1'>
           <ul className='flex flex-wrap justify-start w-auto gap-1'>
-            <li className='flex items-center justify-center px-1.5 h-7 text-xs'>authors:</li>
-            {
-              result["authors"].toString().replaceAll(" and ", ", ").split(", ").map((author: string) => (
-                <li className='flex items-center justify-center px-1.5 h-7 rounded-lg border-[1px] text-xs' key={author}>{author}</li>
-              ))
-            }
-          </ul>
-          <ul className='flex flex-wrap justify-start w-auto gap-1'>
             <li className='flex items-center justify-center px-1.5 h-7 text-xs'>categories:</li>
             {
               result["categories"].toString().split(" ").map((cat: string) => (
@@ -36,6 +28,17 @@ export const RealSearchResult = (props: any) => {
             <li className='flex items-center justify-center px-1.5 h-7 text-xs'>arXiv ID:</li>
             <li className='flex items-center justify-center px-1.5 h-7 rounded-lg border-[1px] text-xs'>{result["paperId"]}</li>
           </ul>
+          {
+            isExpand ?
+            <ul className='flex flex-wrap justify-start w-auto gap-1'>
+              <li className='flex items-center justify-center px-1.5 h-7 text-xs'>authors:</li>
+              {
+                result["authors"].toString().replaceAll(" and ", ", ").split(", ").map((author: string) => (
+                  <li className='flex items-center justify-center px-1.5 h-7 rounded-lg border-[1px] text-xs' key={author}>{author}</li>
+                ))
+              }
+            </ul> : null
+          }
         </div>
         <div className='flex items-start justify-center pt-1'>
           <button className='w-6 h-6 text-gray-500' onClick={() => setIsExpand(!isExpand)}>
@@ -45,17 +48,40 @@ export const RealSearchResult = (props: any) => {
       </div>
       <Link
         href={`/paper?paperId=${result["paperId"]}&query=${query}`}
-        className="flex items-center justify-start text-left w-full px-1.5 text-base font-semibold pb-3 text-cyan-900 hover:underline"
+        className="flex items-center justify-start text-left w-full px-1.5 text-base font-semibold pb-3 text-cyan-800 hover:underline"
       >
         <h3>
           {result['title']}
         </h3>
       </Link>
+      <div className='flex flex-col show-logo:flex-row items-start justify-center text-left w-full px-1.5 text-sm pb-3 gap-3'>
+        <h5>Explaination:</h5>
+        <p className="font-extralight">
+          {
+            (() => {
+              const res: [string] = result['abstract'].split(" ");
+              return <>
+                {
+                  res.slice(0, Math.min(res.length, isExpand ? res.length : 50))
+                    .join(" ") + 
+                    ((!isExpand) && res.length > 50 ? " ... " : "")
+                }
+                {
+                  (!isExpand) && res.length > 50 ?
+                  <a className="text-cyan-800 font-normal hover:underline hover:cursor-pointer" onClick={() => setIsExpand(true)}>
+                    more
+                  </a> : null
+                }
+              </>;
+            })()
+          }
+        </p>
+      </div>
       {
         isExpand ?
           <>
             <div className='flex flex-col show-logo:flex-row items-start justify-center text-left w-full px-1.5 text-sm pb-3 gap-3'>
-              <h5>Abstract:</h5>
+              <h5>Abstract:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h5>
               <p className="font-extralight">
                 {result['abstract']}
               </p>
