1cf58f047f821b51881de19bc120d2dc21f47529
ESRA <MY_NAME@example.com>
2023-02-05 05:46:14 +0000

    [ADD] 2 gpu load balancer and rate limit

--- cd.sh
+++ cd.sh
@@ -2,5 +2,6 @@ docker build -t esra-backend ./esra-backend  && \
 docker build -t esra-frontend ./esra-frontend  && \
 docker build -t esra-paper ./esra-paper  && \
 docker build -t esra-search ./esra-search  && \
+docker build -t esra-search-load-balancer ./load-balancer  && \
 docker build -t esra-nginx ./api-gateway  && \
 docker-compose up -d
